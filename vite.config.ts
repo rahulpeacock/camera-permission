09b8d495ce2db7e801f1f3877aa47b4b1cd71a2b
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-07-16 21:41:51 +0530

    ci: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,20 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite(), tailwindcss()],
+  server: {
+    https: {
+      key: './cert/localhost-key.pem',
+      cert: './cert/localhost.pem',
+    },
+    port: 3000,
+  },
+  preview: {
+    https: {
+      key: './cert/localhost-key.pem',
+      cert: './cert/localhost.pem',
+    },
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

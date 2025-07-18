e8515625e68900637b0bbce4be039b00b939b81b
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-07-18 17:35:32 +0530

    chore: update the content

--- src/features/media-stream/components/microphone-permission.tsx
+++ src/features/media-stream/components/microphone-permission.tsx
@@ -1,6 +1,7 @@
 import { SemiCircularLoader } from '@/features/global/components/loader';
 import type { PermissionQueryError, PermissionQueryErrorType } from '@/lib/types';
 import React from 'react';
+import { MicrophoneGranted } from './microphone-granted';
 import { MicrophonePrompt } from './microphone-prompt';
 
 const PERMISSION_NAME = 'microphone' as PermissionName;
@@ -68,27 +69,25 @@ export function MicrophonePermission() {
   }
 
   if (permissionState === 'prompt') {
-    return (
-      <div>
-        <MicrophonePrompt />
-      </div>
-    );
+    return <MicrophonePrompt />;
   }
 
   if (permissionState === 'granted') {
-    return (
-      <div>
-        <h2>Permission Granted</h2>
-        <p>UI for permission granted</p>
-      </div>
-    );
+    return <MicrophoneGranted />;
   }
 
   if (permissionState === 'denied') {
     return (
-      <div>
-        <h2>Permission Denied</h2>
-        <p>UI for permission denied</p>
+      <div className='min-h-screen flex items-center justify-center text-center'>
+        <div>
+          <h2 className='text-xl font-semibold'>The application tried to access your microphone, but it was blocked</h2>
+          <p className='text-sm max-w-[800px] mt-1.5'>
+            This error occurs when the browser does not have permission to access the microphone, due to user denial. Don't worry, this is a common
+            issue and can be easily fixed by granting the necessary permissions. To resolve this, please check your browser's settings to ensure
+            microphone access is allowed for this site. You can also click the "Allow" button if prompted by your browser when attempting to use the
+            microphone.
+          </p>
+        </div>
       </div>
     );
   }

a0a23adc698565afeb0481074e8ca92bcd0ba0ef
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-07-20 21:31:01 +0530

    update: error messages for microphone-denied

--- src/features/media-stream/components/microphone-granted.tsx
+++ src/features/media-stream/components/microphone-granted.tsx
@@ -1,4 +1,21 @@
+import React from 'react';
+
 export function MicrophoneGranted() {
+  const [streamLoading, setStreamLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function getMicrophoneStream() {
+      try {
+        const _localStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      } catch (err) {
+        console.log('Error: ', err);
+      } finally {
+        setStreamLoading(false);
+      }
+    }
+    getMicrophoneStream();
+  }, []);
+
   return (
     <div>
       <p>Microphone granted</p>
